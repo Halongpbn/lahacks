1c95abebf78cd2a6a1b26e7dbd751238d5177914
ValerieCHoi <32984644+ValerieCHoi@users.noreply.github.com>
2018-03-25 02:33:10 -0700

    Create about.js
    Should be able to have the menu button

---
+++ about.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
