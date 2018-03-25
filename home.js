c80c106652105a296ca37037da98a36611261988
Alyssa Nguyen <halongpbn@gmail.com>
2018-03-25 04:06:51 -0700

    Update home.js

--- home.js
+++ home.js
@@ -10,7 +10,7 @@ function closeNav() {
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.7749, lng: -122.4194},
-    zoom: 15,
+    zoom: 11,
     disableDefaultUI: true,
     styles: [
           {
@@ -190,8 +190,6 @@ function initMap() {
           }
         ]
   });
-    
-
   var layer = new google.maps.FusionTablesLayer({
     query: {
       select: 'Location',
@@ -199,4 +197,32 @@ function initMap() {
     }
   });
   layer.setMap(map);
-}
+  infoWindow = new google.maps.InfoWindow;
+
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          //infoWindow.open(map);
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    }
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+            infoWindow.setPosition(pos);
+            infoWindow.setContent(browserHasGeolocation ?
+                                  'Error: The Geolocation service failed.' :
+                                  'Error: Your browser doesn\'t support geolocation.');
+            infoWindow.open(map);
+          }
